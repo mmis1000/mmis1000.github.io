0be6ab4126b2ae22737ddf351c27b695605360fd
mmis1000 <mmis10002@gmail.com>
2022-12-21 18:48:20 +0800

    Update footer time

--- components/layout/layout-footer.vue
+++ components/layout/layout-footer.vue
@@ -2,7 +2,7 @@
   <footer id="copyright" class="page footer copyright">
     <h3>About this Page</h3>
     <p>
-      Last edit: <time datetime="2018-2-11 21:35">2018/02/11</time><br>
+      Last edit: <time datetime="2018-2-11 21:35">2022/12/21</time><br>
       created by <a id="author" href="http://mmis1000.me/" rel="author">mmis1000</a> <br>
       Code release under <a href="https://en.wikipedia.org/wiki/MIT_License">MIT License</a>
     </p>
