5baf85a374dc8061fc4f38941aeae4257f4b441d
mmis1000 <mmis10002@gmail.com>
2022-12-21 18:46:41 +0800

    Rewrite with vue

---
+++ components/section/section-about.vue
@@ -0,0 +1,334 @@
+<template>
+  <header class="page about-wrapper" ref="hello">
+    <div class="about-outer">
+      <div class="about-inner">
+        <i class="fa fa-user fa-5x avatar" :class="avatarClass"></i>
+        <h1>Mmis1000</h1>
+        <p class="links">
+          <a href="https://mmis1000.me/blog"><button class="button">Blog</button></a>
+          <a href="https://t.me/mmis1000"><button class="button">Telegram</button></a>
+        </p>
+        <p v-if="!ghostWoken" class="cursor target">
+          Hello
+          visitor!
+          I am mmis1000.
+          who learning the web technology and skills.
+        </p>
+        <p v-else class="cursor target" :style="currentStyle" v-html="ghostWritingHTML">
+        </p>
+        <button class="control play" :class="{
+          hide: !mounted,
+          play: !ghostWoken,
+          stop: ghostWoken
+        }" @click="ghostWoken ? onStop() : onPlay()"></button>
+      </div>
+    </div>
+    <Teleport to="body">
+      <div v-show="popupVisible" id="popup" class="popup">Hey, I am a popup!</div>
+    </Teleport>
+  </header>
+</template>
+<script lang="ts" setup>
+import imgUrl from '~~/assets/example.gif'
+const currentStyle: Record<string, string | undefined> = reactive({})
+const hello = ref<HTMLElement>()
+const popupVisible = ref(false)
+const avatarClass = ref<string[]>([])
+const ghostWoken = ref(false)
+const ghostWritingHTML = ref('')
+const mounted = ref(false)
+const actions = ScriptBuilder.create()
+  .effect(() => {}, async () => {
+    hello.value?.focus()
+  })
+  .text(`Hello
+          visitor!
+          I am mmis1000`)
+  .changeInterval(500)
+  .text('...')
+  .changeInterval(50)
+  .text('who learning the web technology and skills.')
+  .delay(2000)
+  .clear()
+
+  .text('Let\'s talk about what am I learning.')
+  .text('<br>The main components of a web pages are HTML, CSS and JS.')
+  .delay(1000)
+  .clear()
+
+  .text('The HTML create the page and text you saw now. ')
+  .text('<br> For example: add an Image ')
+  .text(`<br><img src="${imgUrl}"> `)
+  
+  .delay(5000)
+  .deleteLine(2)
+
+  .text('<br>Or create a list ')
+  .text(`<br>
+      <ol>
+        <li>I</li>
+        <li>am</li>
+        <li>a</li>
+        <li>list.</li>
+      </ol>
+    `)
+  .delay(5000)
+  .deleteLine(2)
+
+  .text('<br> Or make a link ')
+  .text(`<br>
+      <a href="javascript:void(0)">Click me please.</a>
+    `)
+  .delay(5000)
+  .clear()
+
+  .text('The CSS stylize the text and page. ')
+  .delay(1000)
+  .text('<br>For Example: Make the Text Red ')
+  .effect(
+    () => { currentStyle.color = undefined },
+    async () => { currentStyle.color = 'red' }
+  )
+  .text('<br>Make the Text Bold ')
+  .effect(
+    () => { currentStyle['font-weight'] = undefined },
+    async () => { currentStyle['font-weight'] = 'bold' }
+  )
+  .text('<br>Make the Text Big ')
+  .effect(
+    () => { currentStyle['font-size'] = undefined },
+    async () => { currentStyle['font-size'] = '1.5em' }
+  )
+  .delay(1000)
+  .effect(
+    () => {},
+    async () => {
+      currentStyle.color = undefined
+      currentStyle['font-weight'] = undefined
+      currentStyle['font-size'] = undefined
+    }
+  )
+  .clear()
+
+  .text('The CSS can easily control how the page should display ')
+  .text('<br>And make web colorful. ')
+  .delay(1000)
+  .clear()
+
+  .text('And the JS animate the web. ')
+  .effect(
+    () => { avatarClass.value = [] },
+    async () => {
+      avatarClass.value = ['avatar--delay-250']
+      await nextTick()
+      avatarClass.value = ['avatar--delay-250', 'avatar--left']
+      await new Promise(resolve => setTimeout(resolve, 250))
+      avatarClass.value = ['avatar--delay-500', 'avatar--left']
+      await nextTick()
+      avatarClass.value = ['avatar--delay-500', 'avatar--right']
+      await new Promise(resolve => setTimeout(resolve, 500))
+      avatarClass.value = ['avatar--delay-250', 'avatar--right']
+      await nextTick()
+      avatarClass.value = ['avatar--delay-250', 'avatar--middle']
+      await new Promise(resolve => setTimeout(resolve, 250))
+    }
+  )
+  .text('<br>Interact with user. ')
+  .delay(1000)
+  
+  .effect(
+    () => { popupVisible.value = false },
+    async () => { popupVisible.value = true }
+  )
+  .delay(1000)
+  .effect(
+    () => {},
+    async () => { popupVisible.value = false }
+  )
+  .text('<br>Make all kinds of magic happens. ')
+  .delay(1000)
+  .clear()
+
+  .text('These are all I am learning now. ')
+  .text('<br>Thank you for your listening. ')
+  .text('<br>Goodbye!!! ')
+  .delay(3000)
+  .build()
+
+let writer: InstanceType<typeof GhostTyping> | null = null
+
+const onPlay = () => {
+  writer?.start()
+}
+const onStop = () => {
+  writer?.stop()
+}
+
+onMounted(() => {
+  mounted.value = true
+  writer = new GhostTyping(actions)
+  writer.onState(state => {
+    ghostWoken.value = state === 'START'
+  })
+  writer.onText(text => {
+    ghostWritingHTML.value = text
+  })
+})
+</script>
+<style scoped>
+.avatar {
+  position : relative;
+}
+.avatar--delay-250 {
+  transition: all .25s;
+}
+.avatar--delay-500 {
+  transition: all .5s;
+}
+.avatar--left {
+  left: -50px;
+}
+.avatar--right {
+  left: 50px;
+}
+.avatar--middle {
+  left: 0;
+}
+
+.about-wrapper {
+  height:100%;
+  display:table;
+  
+  background:black;
+  background:url(~/assets/bg.jpg);
+  background-size: auto, 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  color: white;
+}
+.about-outer {
+  display:table-cell;
+  vertical-align:middle;
+}
+.about-inner {
+  margin-left:auto;
+  margin-right:auto;
+  width:80%;
+  text-align:center;
+  -webkit-text-shadow:0px 2px 10px #000000;
+     -moz-text-shadow:0px 2px 10px #000000;
+          text-shadow:0px 2px 10px #000000;
+}
+.about-inner .links a::before {
+  display: none;
+}
+
+
+.cursor:after{
+  content: "|";
+  
+  -webkit-animation-name: blink;
+  -webkit-animation-duration: 0.8s;
+  -webkit-animation-iteration-count: infinite;
+  -webkit-animation-timing-function: linear;
+  -webkit-animation-timing-function: linear, steps(2, end);
+  
+  animation-name: blink;
+  animation-duration: 0.8s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  animation-timing-function: linear, steps(2, end);
+}
+
+.links > a {
+  margin-left: 4px;
+}
+.links > a:first-child {
+  margin-left: 0;
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes blink {
+  0% {opacity: 1;}
+  50% {opacity: 0;}
+  100% {opacity: 1;}
+}
+/* Standard syntax */
+@keyframes blink {
+  0% {opacity: 1;}
+  50% {opacity: 0;}
+  100% {opacity: 1;}
+}
+
+.control {
+  background: transparent;
+  border: 1px solid #bbbbbb;
+  padding: 0.5em;
+  border-radius: 6px;
+  behavior: url(border-radius.htc); /* IE 6-8 */
+
+  -webkit-text-shadow:0px 2px 10px #000000;
+     -moz-text-shadow:0px 2px 10px #000000;
+          text-shadow:0px 2px 10px #000000;
+}
+.control.hide {
+  display: none;
+}
+.control:before {
+  font-family: FontAwesome;
+  font-size : 0.8em;
+  vertical-align:middle;
+  display: inline-block;
+  padding-right: 3px;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.8;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.control.play {
+  animation: infinite 2s blink;
+}
+.control.play:before {
+  content: "\f04b";
+  color: #4f4;
+}
+.control.play:after {
+  content: "Tell me about you";
+  color: #4f4;
+}
+
+.control.stop:before {
+  content: "\f04d";
+  color: #f44;
+}
+.control.stop:after {
+  content: "stop";
+  color: #f44;
+}
+
+.popup {
+  display: block;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  margin-left: -100px;
+  margin-top: -50px;
+  width: 200px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  background: #fefefe;
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, 0.32) 0px 3px 6px;
+  z-index: 1;
+}
+</style>
---
+++ components/section/section-contact.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="contact page">
+    <h2>To Contact <i class="fa fa-reply"></i></h2>
+    <section class="contact-method github">
+      <i class="fa fa-github fa-5x"></i>
+      <h3>Github</h3>
+      <p>GitHub is a Web-based Git repository hosting service. It offers all of the distributed revision control and
+        source code management (SCM) functionality of Git as well as adding its own features.</p>
+      <p><a href="https://github.com/mmis1000">Mmis1000</a></p>
+    </section>
+    <section class="contact-method facebook">
+      <i class="fa fa-facebook fa-5x"></i>
+      <h3>Facebook</h3>
+      <p>Facebook is an online social networking service headquartered in Menlo Park, California. Its website was
+        launched on February 4, 2004, by Mark Zuckerberg with his Harvard College roommates and fellow students Eduardo
+        Saverin, Andrew McCollum, Dustin Moskovitz and Chris Hughes.</p>
+      <p><a href="https://www.facebook.com/mmis1000">Mmis1000</a></p>
+    </section>
+    <section class="contact-method email">
+      <i class="fa fa-envelope fa-5x"></i>
+      <h3>Email</h3>
+      <p>Electronic mail, most commonly referred to as email or e-mail since around 1993, is a method of exchanging
+        digital messages from an author to one or more recipients. Email operates across the Internet or other computer
+        networks.</p>
+      <p><a href="mailto:mmis1000@yahoo.com.tw">Mmis1000</a></p>
+    </section>
+  </section>
+</template>
+<style scoped>
+.contact {
+  color: #f7f7f7;
+  background: #0277BD;
+  text-align: center;
+  
+  padding-top:2em;
+  padding-bottom:2em;
+}
+
+.contact .contact-method {
+  vertical-align:top;
+  width: 95%;
+  display: inline-block;
+  /*height: 200px;*/
+  border-top: #777777 solid 1px;
+  padding-top:1em;
+  padding-bottom:1em;
+}
+
+@media screen and (min-width: 600px) {
+  .contact .contact-method {
+    border-top: none;
+  }
+}
+</style>
